30f81d25253cd2c587d0a7b977e7304e5f3065f5
Romadhan Prasetyo <dyprasetyox@gmail.com>
2020-10-13 13:23:08 +0700

    Update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 export default {
-    // Global page headers (https://go.nuxtjs.dev/config-head)
     loading: '~/components/Preloader.vue',
+    
+    // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+    ssr: false,
+
+    // Target (https://go.nuxtjs.dev/config-target)
+    target: 'static',
+    // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
       title: 'Dygoo',
       meta: [
